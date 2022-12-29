230f86019ceb4f7cba59ec9528d7f043c9e4bab4
yushin <am1213@naver.com>
2022-12-29 23:34:38 +0900

    #LOLMATCHER-32 # member 엔티티 수정

--- src/main/kotlin/com/lolmatch/team/domain/Member.kt
+++ src/main/kotlin/com/lolmatch/team/domain/Member.kt
@@ -10,24 +10,24 @@ import javax.persistence.*
 @DynamicUpdate
 @Entity
 @Table(name = "tbl_member")
-class Member : BaseEntity() {
+class Member(
+    @Column(name = "m_role_type")
+    @Enumerated(EnumType.STRING)
+    var mRoleType: MemberRoleType = MemberRoleType.MEMBER,
+
+    @ManyToOne
+    @JoinColumn(name = "team_seq")
+    var team : Team? = null,
+
+    @OneToOne
+    @JoinColumn(name = "user_seq")
+    var user: User? = null
+) : BaseEntity() {
 
     @Id
     @Column(name = "member_seq")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var memberSeq: Long = 0L
-
-    @Column(name = "m_role_type")
-    @Enumerated(EnumType.STRING)
-    var mRoleType: MemberRoleType = MemberRoleType.MEMBER
-
-    @ManyToOne
-    @JoinColumn(name = "team_seq")
-    var team : Team? = null
-
-    @OneToOne
-    @JoinColumn(name = "user_seq")
-    var user: User? = null
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
@@ -35,5 +35,5 @@ class Member : BaseEntity() {
         joinColumns = [JoinColumn(name = "member_seq")],
         inverseJoinColumns = [JoinColumn(name = "mrole_seq")]
     )
-    val mRoles: Set<Mrole>? = mutableSetOf()
+    val mRoles: MutableSet<Mrole>? = mutableSetOf()
 }
